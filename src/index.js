6001ee24252a057a441c3b352c79f4339a75978d
Svetlana Drozdova <sdrozdova032@gmail.com>
2021-08-18 15:41:22 +0400

    init: write functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-
+let minValue = 0;
 exports.min = function min (array) {
-  return 0;
+  if (array.length === 0) {
+    return 0;
+  } else {
+    let sortArr = array.sort((a, b) => a -b);
+     minValue = sortArr[0];
+  }
+  return minValue;
 }
-
+let maxValue = 0;
 exports.max = function max (array) {
-  return 0;
+  if (array.length === 0) {
+    return 0;
+  } else {
+    let sortArr = array.sort((a, b) => a -b);
+    maxValue = sortArr[sortArr.length - 1];
+  }
+  return maxValue;
 }
-
+let avgValue = 0;
 exports.avg = function avg (array) {
-  return 0;
+  if(array.length === 0){
+    return 0;
+  } else {
+    let myCallback = ( total, currentValue, currentIndex, arr ) => {  
+      return total + currentValue; 
+    }
+    let result = array.reduce(myCallback);
+    avgValue = result/array.length;
+  }
+  return avgValue;
 }
